8a7d35315d724bc529c966ab03846b6b37ef8218
Ivan Lee <ilee.code@gmail.com>
2020-08-18 22:56:10 -0700

    Created win condition for player o using hasClass

--- tictactoe/js/script.js
+++ tictactoe/js/script.js
@@ -13,8 +13,16 @@ $(document).ready(function(){
   var spot9 = $('#spot9');
 
   $('#board li').on('click',function(){
-    if($()){
-
+    if(spot1.hasClass('o') && spot2.hasClass('o') && spot3.hasClass('o') ||
+    spot4.hasClass('o') && spot5.hasClass('o') && spot6.hasClass('o') ||
+    spot7.hasClass('o') && spot8.hasClass('o') && spot9.hasClass('o') ||
+    spot1.hasClass('o') && spot4.hasClass('o') && spot7.hasClass('o') ||
+    spot2.hasClass('o') && spot5.hasClass('o') && spot8.hasClass('o') ||
+    spot3.hasClass('o') && spot6.hasClass('o') && spot9.hasClass('o') ||
+    spot1.hasClass('o') && spot5.hasClass('o') && spot9.hasClass('o') ||
+    spot3.hasClass('o') && spot5.hasClass('o') && spot7.hasClass('o') 
+    ){
+      alert("Winner: o")
     }
   })
 })
