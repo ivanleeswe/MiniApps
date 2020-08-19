5d1b8f0f2bbd73496477f82f71a3452441249cac
Ivan Lee <ilee.code@gmail.com>
2020-08-18 22:49:32 -0700

    Created variables for each spot

---
+++ tictactoe/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  var x = "x";
+  var o = "o";
+  var turns = 0;
+  var spot1 = $('#spot1');
+  var spot2 = $('#spot2');
+  var spot3 = $('#spot3');
+  var spot4 = $('#spot4');
+  var spot5 = $('#spot5');
+  var spot6 = $('#spot6');
+  var spot7 = $('#spot7');
+  var spot8 = $('#spot8');
+  var spot9 = $('#spot9');
+
+  $('#board li').on('click',function(){
+    if($()){
+
+    }
+  })
+})
