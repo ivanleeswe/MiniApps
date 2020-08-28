4201110601a7edcffc5d5242099f176661b30505
Ivan Lee <ilee.code@gmail.com>
2020-08-27 22:50:27 -0700

    Updated toggle function to hide previously clicked answers when a new one is clicked

--- faqslider/js/script.js
+++ faqslider/js/script.js
@@ -5,6 +5,14 @@ var speed= "500";
 $(document).ready(function(){
   $('li.q').on(action, function() { //li is not a class, its an element
     $(this).next()
-      .slideToggle(speed);  //this refers to q you click on, next element is gonna be answer
-  })                        //jquery have slide up slide down toggle function
+      .slideToggle(speed)
+        .siblings('li.a')
+          .slideUp();
+  });
 });
+
+//this refers to q you click on, next element is gonna be answer
+//jquery have slide up slide down toggle function
+//we are chaining here in this function
+//siblings (pick all a)
+//now once you opesns, it will close the previous opened ones
